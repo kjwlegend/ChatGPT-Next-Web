3eaed4f2dcb62106460f2bad86e8698e6704c5e8
jingwei kong <56349014@qq.com>
2023-11-16 02:38:00 +0800

    small fix and update template

--- app/auth/code-login.tsx
+++ app/auth/code-login.tsx
@@ -4,71 +4,71 @@ import { Button, Checkbox, Form, Input } from "antd";
 import { useAccessStore, useUserStore } from "../store";
 
 export default function CodeLogin() {
-  const router = useRouter();
-  const access = useAccessStore();
-  const useStore = useUserStore();
+	const router = useRouter();
+	const access = useAccessStore();
+	const useStore = useUserStore();
 
-  const goHome = () => router.replace("/");
-  const path = usePathname();
+	const goHome = () => router.replace("/");
+	const path = usePathname();
 
-  const onFinish = (values: any) => {
-    const code = values.password;
-    console.log("Received values of form: ", code);
-    access.updateCode(code);
-    useStore.updateNickname(values.nickname);
-    goHome();
-    console.log(path);
-  };
+	const onFinish = (values: any) => {
+		const code = values.password;
+		console.log("Received values of form: ", code);
+		access.accessCode = code;
+		useStore.updateNickname(values.nickname);
+		goHome();
+		console.log(path);
+	};
 
-  return (
-    <div>
-      <Form
-        name="quick_login"
-        className="login-form"
-        initialValues={{
-          nickname: useStore.user.nickname,
-          password: access.accessCode,
-        }}
-        style={{ maxWidth: 900, minWidth: 300 }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="nickname"
-          rules={[
-            {
-              required: false,
-              message: "请输入密码",
-            },
-          ]}
-          hasFeedback
-        >
-          <Input placeholder="昵称" />
-        </Form.Item>
+	return (
+		<div>
+			<Form
+				name="quick_login"
+				className="login-form"
+				initialValues={{
+					nickname: useStore.user.nickname,
+					password: access.accessCode,
+				}}
+				style={{ maxWidth: 900, minWidth: 300 }}
+				onFinish={onFinish}
+			>
+				<Form.Item
+					name="nickname"
+					rules={[
+						{
+							required: false,
+							message: "请输入密码",
+						},
+					]}
+					hasFeedback
+				>
+					<Input placeholder="昵称" />
+				</Form.Item>
 
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "请输入密码",
-            },
-          ]}
-          hasFeedback
-        >
-          <Input.Password placeholder="请输入授权码" />
-        </Form.Item>
+				<Form.Item
+					name="password"
+					rules={[
+						{
+							required: true,
+							message: "请输入密码",
+						},
+					]}
+					hasFeedback
+				>
+					<Input.Password placeholder="请输入授权码" />
+				</Form.Item>
 
-        <Form.Item>
-          <Button
-            block
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
-          >
-            {Locale.Auth.Confirm}
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
+				<Form.Item>
+					<Button
+						block
+						type="primary"
+						htmlType="submit"
+						className="login-form-button"
+					>
+						{Locale.Auth.Confirm}
+					</Button>
+				</Form.Item>
+			</Form>
+		</div>
+	);
 }
