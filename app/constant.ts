c3b1735c13ea8577dc3b307ddcd84598eccabc4f
jingwei kong <56349014@qq.com>
2024-05-28 13:20:15 +0800

    update env

--- app/constant.ts
+++ app/constant.ts
@@ -178,8 +178,8 @@ export const oss = "https://xiaoguangai.oss-cn-shanghai.aliyuncs.com";
 // console.log("env", env);
 
 // export const server_url = env;
-// export const server_url = "https://admin.xiaoguang.fun";
-export const server_url = "http://localhost:8000";
+export const server_url = "https://admin.xiaoguang.fun";
+// export const server_url = "http://localhost:8000";
 
 export const version = "2.4.2";
 
