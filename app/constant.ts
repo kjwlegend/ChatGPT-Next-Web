c197e852ff8e0f99567c25a5f7c90833c2d47aba
jingwei kong <56349014@qq.com>
2024-05-29 11:04:23 +0800

    release: 2.4.3

--- app/constant.ts
+++ app/constant.ts
@@ -177,11 +177,11 @@ export const oss = "https://xiaoguangai.oss-cn-shanghai.aliyuncs.com";
 // const env = getServerSideConfig().server_url;
 // console.log("env", env);
 // export const server_url = env;
-// export const server_url = "https://admin.xiaoguang.fun";
+export const server_url = "https://admin.xiaoguang.fun";
 
-export const server_url = "http://localhost:8000";
+// export const server_url = "http://localhost:8000";
 
-export const version = "2.4.2";
+export const version = "2.4.3";
 
 export const CHAT_PAGE_SIZE = 15;
 export const MAX_RENDER_MSG_COUNT = 45;
