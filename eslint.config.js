fab6eef1eb123dc688d50b464a40f1506846490a
jingwei kong <56349014@qq.com>
2024-06-12 11:33:15 +0800

    chore: update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,7 @@
 // eslint.config.js
 //TODO 
 // 更新到 ESLINT 9.4
-import eslintPluginPrettier from "eslint-plugin-prettier"
-
-export default [
-  {
-    files: ["**/*.js"],
-    plugins: {
-      prettier: eslintPluginPrettier
-    },
-    rules: {
-
-    },
-    ignorepatterns: ["public/serviceWorker.js"]
-  }
-]
+module.exports = {
+  extends: ['next/core-web-vitals'],
+  plugins: ['prettier'],
+}
